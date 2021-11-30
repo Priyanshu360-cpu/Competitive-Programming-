a40d1046434bec56f72db7382749863e857c1006
Priyanshu360-cpu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2021-11-30 14:06:21 +0530

    Update patterns.c

--- C/patterns.c
+++ C/patterns.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#define newl printf("\n");
 int main(void) {
     int n;
     scanf("%d",&n); 
@@ -7,14 +7,14 @@ int main(void) {
    for(int j=1;j<=i;j++){ 
        printf("*"); 
    }
-   printf("\n"); 
+   newl 
     }
-    printf("\n");  
+    newl 
     for(int i=n;i>=1;i--){ 
         for(int j=1;j<=i;j++){ 
             printf("*"); 
         }
-        printf("\n"); 
+        newl 
     } 
     printf("\n");
     for(int i=n;i>=1;i--) {
@@ -24,14 +24,14 @@ int main(void) {
         for(int k =1;k<=i;k++) { 
             printf("*"); 
         }
-        printf("\n");
+        newl 
     }
     printf("\n"); 
      for(int i=1;i<=n;i++){ 
          for(int j=i;j>=1;j--) { 
              printf("%d",j);
          }
-         printf("\n");
+         newl 
      }
      printf("\n");
      for(int i=1;i<=n;i++) {
@@ -41,7 +41,7 @@ int main(void) {
          for(int j=1;j<=i;j++) { 
              printf("1 ");
          }
-         printf("\n");
+         newl 
      }
      printf("\n");
      for(int i=1;i<=n;i++) {
@@ -53,7 +53,7 @@ int main(void) {
                  printf("1 ");
              }
          }
-         printf("\n");
+         newl 
      }
      printf("\n");
      for(int i=1;i<=n;i++) {
@@ -63,7 +63,7 @@ int main(void) {
          for(int j=1;j<=i;j++) {
              printf("1 ");
          }
-         printf("\n"); 
+         newl 
      }
      for(int i=n-1;i>=1;i--) {
          for(int k=n-i;k>=1;k--) { 
@@ -72,7 +72,7 @@ int main(void) {
          for(int j=1;j<=i;j++) {
              printf("1 "); 
          }
-         printf("\n");
+         newl
      }
      printf("\n*\n"); 
      for(int i=1;i<=n;i++) {
@@ -80,9 +80,9 @@ int main(void) {
          for(int k=1;k<=i;k++) {
              printf(" "); }
              printf("*");
-             printf("\n"); 
+             newl 
      }
-     for(int j=1;j<=n+2;j++) { 
+     for(int j=1;j<=n+3;j++) { 
          printf("*");
      }
      printf("\n\n");
@@ -93,7 +93,7 @@ int main(void) {
              printf("%d",a);
              a=a+1; 
          }
-         printf("\n"); 
+         newl 
      }
      a=1;
          printf("\n");
@@ -113,7 +113,7 @@ int main(void) {
                  }
                  a=y+1;
              }
-             printf("\n");
+             newl
          }
          printf("\n");
          int s=64;
@@ -122,16 +122,72 @@ int main(void) {
              for(int j=1;j<=i;j++) { 
                  printf("%c",s);
              }
-             printf("\n");
+             newl
          }
-                 printf("\n");
+                 newl
                  s=64;
                  for(int i=1;i<=n;i++) {
                      for(int j=1;j<=i;j++) {
                          s=s+1;
                          printf("%c",s); }
                          s=64;
-                         printf("\n");
+                         newl
                  }
-     
-}
+                 newl 
+                 s=((n*(n+1))/2);
+                 for(int i =n;i>=1;i--){
+                     for(int j=1;j<=i;j++){
+                         printf("%d ",s);
+                         s=s-1;
+                     }
+                     newl
+                 }
+     newl
+     for(int i =1;i<=n;i++){
+         for(int j=1;j<=i;j++){
+             printf("*");
+         }
+         newl
+     }
+     for(int i =n;i>=1;i--){
+         for(int j=1;j<=i;j++){
+             printf("*");
+         }
+         newl
+     }
+     newl
+     int u=1;
+     for(int i =n;i>=1;i--){
+         for(int k=1;k<=u-1;k++){
+             printf("o");
+         }
+         printf("*");
+         for(int j =i;j>=1;j--){
+             printf("=");
+         }
+         printf("*");
+             for(int p=1;p<=u-1;p++){
+                 printf("#");
+             }
+         
+         u=u+1;
+         newl
+     } 
+     u=1;
+   for(int i =n;i>=1;i--){
+         for(int k=1;k<=i;k++){
+             printf("o");
+         }
+         printf("*");
+         for(int j =1;j<=u;j++){
+             printf("@");
+         }
+         printf("*");
+             for(int p=1;p<=i;p++){
+                 printf("#");
+             }
+         
+         u=u+1;
+         newl
+     }
+      }
