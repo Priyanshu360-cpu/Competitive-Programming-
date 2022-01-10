2843504d6ed8b07ee04a94251d44966a033fafe5
Priyanshu360-cpu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-01-10 11:05:17 +0530

    Create function_starter.c

---
+++ C/function_starter.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+float fact(int a){
+    int k=1;
+    float s=0.0;
+    int z;
+    while(k!=a){
+        if(k%2==0){
+            z=1;
+            for(int i=k;i>0;i--){
+                z=z*i;
+            }
+            s=s+z;
+        }
+        else{
+            z=1;
+            for(int i=k;i>0;i--){
+                z=z*i;
+            }
+            
+            s=s+(1.0/z);
+        }
+        k=k+1;
+    }
+    return s;
+}
+void ptrn(int a,char z){
+   for(int i=0;i<a;i++){
+       for(int j=0;j<a-i;j++){
+           printf("%c",z);
+       }
+       printf("\n");
+   }
+}
+int calc(int x,int y,char c){
+    int z;
+    switch(c){
+        case '+':
+         z=x+y;
+         break;
+        case '-':
+         z=x-y;
+         break;
+        case '*':
+         z=x*y;
+         break;
+        case '/':
+         z=x/y;
+         break;
+        default:
+         z=0;
+    }
+    return z;
+}
+void drive(int a,int b,char c){
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            printf("%c",c);
+        }
+        printf("\n");
+    }
+}
+int avg(int x,int y,int z){
+    int c;
+    c=(x+y+z)/3;
+    return c;
+}
+int fibo(int n){
+    if (n <= 1)
+        return n;
+    return fibo(n-1) + fibo(n-2);
+}
+int main(void) {
+    char s[30];
+    scanf("%[^\n]s",s);
+    int k=0;
+    int *p=s;
+    while(s[k]!='\0'){
+        k++;
+    }
+    printf("%d\n",k);
+    while(k){
+        for(int i=0;p[i]!='\0';i++){
+            printf("%c",*(p+i));
+        }
+        printf("\n");
+        k--;
+    }
+    printf("%d\n",calc(2,3,'+'));
+	printf("%f",fact(5));
+	printf("\n");
+	ptrn(3,'=');
+	drive(2,3,'<');
+	printf("\n");
+	printf("%d",fibo(32));
+	printf("\n");
+	printf("%d",avg(2,3,4));
+	return 0;
+}
