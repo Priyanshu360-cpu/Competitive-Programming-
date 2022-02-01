bc5ee59b7b565a0f649644ecb5c104d655587b40
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-02-01 19:07:54 +0530

    Update Leetcode- 2linkedlist reversed sum.cpp

--- Cpp/Leetcode- 2linkedlist reversed sum.cpp
+++ Cpp/Leetcode- 2linkedlist reversed sum.cpp
@@ -11,16 +11,15 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* last = NULL;
-        
-        ListNode** head_ref = NULL;
+         ListNode* last;
+       
      
       int c=1;
         int s=0;
         int c2=1;
         int s2=0;
         int t;
-        ListNode* l3;
+       
         
            while(l1->next){
                s=s+((l1->val)*c);
@@ -38,15 +37,15 @@ public:
         printf("%d\n",t);
 
         while(t){
-         
+          ListNode* l3;
+            l3 = (struct ListNode*)malloc(sizeof(struct ListNode));
             l3->val=t%10;
-            l3->next=(*head_ref);
-            (*head_ref)=l3;
+            l3->next=last;
+            last=l3;
+           
             t=t/10;
         }
-         last=l3;
-        printf("%d\n",last->val);
-        l3->next=NULL;
-        return l3;
+         
+        return last;
     }
 };
