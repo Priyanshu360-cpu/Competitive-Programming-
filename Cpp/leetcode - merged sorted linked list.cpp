c6e1047893f7596513e22d8b0fe38d0674fcd837
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-02-02 22:42:44 +0530

    Update leetcode - merged sorted linked list.cpp

--- Cpp/leetcode - merged sorted linked list.cpp
+++ Cpp/leetcode - merged sorted linked list.cpp
@@ -13,23 +13,35 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         vector<int> x;
         vector<int> a;
+       if(list1==NULL&&list2==NULL){
+          return list1;
+       }
+        if(list1==NULL){
+            return list2;
+        }
+        if(list2==NULL){
+            return list1;
+        }
         while(list1->next){
             a.push_back(list1->val);
             list1=list1->next;
         }
+         a.push_back(list1->val);
          while(list2->next){
             a.push_back(list2->val);
             list2=list2->next;
         }
+        a.push_back(list2->val);
        sort(a.begin(), a.end());
-        ListNode* head;
-        for(int i=a.size()-1;i>=0;i++){
-            ListNode* l3;
-            l3 = (struct ListNode*)malloc(sizeof(struct ListNode));
-            l3->val=a[i];
-            l3->next=head;
-            head=l3;
-        }
+          ListNode* head=new ListNode(a[a.size()-1]);
+        a.pop_back();
+       
+      for(int i=a.size()-1;i>=0;i--){
+          ListNode* l3=new ListNode();
+          l3->val=a[i];
+          l3->next=head;
+          head=l3;
+      }
         return head;
     }
 };
