50a5fe2a86977b94527c0d950c6ccad483814c12
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-02-06 21:01:30 +0530

    Create leetcode -2d algo.cpp

---
+++ Cpp/leetcode -2d algo.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i=0;i<matrix.size();i++){
+            if(matrix[i][matrix[i].size()-1]<target) continue;
+            for(int j=0;j<matrix[i].size();j++){
+                if(matrix[i][j]==target) return true;
+            }
+        }
+        return false;
+    }
+};
