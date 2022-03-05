fb80b9aa468bf10e373671de4e4a29e872322fa9
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-03-05 13:18:21 +0530

    Update Variables.asm

--- assembly/Variables.asm
+++ assembly/Variables.asm
@@ -1,19 +1,29 @@
 section	.text
    global _start     
 	
-_start:	            
+_start:	     
+   mov	edx,1   
+   mov ecx,letter
+   mov	ebx,1       
+   mov	eax,4       
+   int	0x80 
+   
+   mov	edx,1   
+   mov ecx,number
+   mov	ebx,1       
+   mov	eax,4       
+   int	0x80 
+   
    mov	edx,9    
-   mov	ecx,letter  
-   mov	ecx,number
    mov ecx,stars
    mov	ebx,1       
    mov	eax,4       
    int	0x80        
 	
-   mov	eax,1       
+   mov	eax,3       
    int	0x80        
 
 section	.data
 letter DB 'y'
-number DW 12345
-stars times 9 db '*'
+number		DW	12345
+stars   times 9 db '*'
