37e379e403fd7a777f55046fd9f0cb1b5b56085d
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-03-10 20:55:10 +0530

    Update Leetcode - unique paths.cpp

--- Cpp/Leetcode - unique paths.cpp
+++ Cpp/Leetcode - unique paths.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         int a=0;
+        if(m<=n){
         for(int i=0;i<n-1;i++){
             a=i+a+(m-1);
             if(i==0)a=a+1;
             cout<<a<<"\n";
+        }
+        }else{
+            
         }
         return a;
         
