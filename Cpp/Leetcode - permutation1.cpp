c7ba578dbd02ff272fcbfcfacd28982b350dfcf6
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-03-13 12:31:13 +0530

    Create Leetcode - permutation1.cpp

---
+++ Cpp/Leetcode - permutation1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> pums=nums;
+        vector<vector<int>> b;
+        b.push_back(pums);
+         next_permutation(pums.begin(),pums.end());
+        while(pums!=nums){
+            b.push_back(pums);
+             next_permutation(pums.begin(),pums.end());
+        }
+        return b;
+    }
+};
