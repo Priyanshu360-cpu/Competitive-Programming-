5e05830b0f41c2c928f1e8ad9a097d3a28270f79
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-03-14 02:26:49 +0530

    Create geekforgeeks - stlfunctions.cpp

---
+++ Cpp/geekforgeeks - stlfunctions.cpp
@@ -0,0 +1,10 @@
+vector<int> sortVector(vector<int>v)
+{
+   sort(v.begin(),v.end());
+    return v;
+}
+vector<int> reverseVector(vector<int>v)
+{
+      reverse(v.begin(),v.end());
+    return v;
+}
