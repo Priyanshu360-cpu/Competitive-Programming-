0163ce1b5e5fd263535e6d8b9d5a742ff30b1d76
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-03-18 19:39:47 +0530

    Create Geekforgeeks trapping water.cpp

---
+++ Cpp/Geekforgeeks trapping water.cpp
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int arr[], int n){
+        int max=arr[0];
+        int sum=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]<max){
+                sum=sum+(max-arr[i]);
+            }
+        }
+        return sum;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+       
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}  // } Driver Code Ends
