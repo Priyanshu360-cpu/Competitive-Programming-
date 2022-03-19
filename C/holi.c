f0f9ce91de3be37deb5156ae38b555bdfcb6ab4d
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-03-19 11:48:42 +0530

    Create holi.c

---
+++ C/holi.c
@@ -0,0 +1,134 @@
+#include<stdio.h> 
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+int main()
+{
+   char a[100]; 
+   printf("\e[1;1H\e[2J");
+   printf("Enter you data ");
+   scanf("%[^\n]s",a);
+   for(int i=0;i<100;i++){
+       if(a[i]=='\0') break;
+       switch(a[i]){
+           case 'H':
+            printf("\033[1;31m");
+            sleep(1);
+            printf("||      ||\n");
+            sleep(1);
+            printf("||      ||\n");
+            sleep(1);
+            printf("||------||\n");
+            sleep(1);
+            printf("||      ||\n");
+            sleep(1);
+            printf("||      ||\n");
+            printf("\033[0m");
+            
+            break;
+           case 'A':
+            printf("\033[0;33m");
+            sleep(1);
+            printf("    /\\    \n");
+            sleep(1);
+            printf("   /  \\    \n");
+            printf("  /----\\    \n");
+            sleep(1);
+            printf(" /      \\    \n");
+            sleep(1);
+            printf("/        \\    \n");
+            printf("\033[0m");
+            
+            break;
+           case 'P':
+            printf("\033[0;36m");
+            sleep(1);
+            printf("----------\n");
+            sleep(1);
+            printf("|         |\n");
+            sleep(1);
+            printf("|         |\n");
+            sleep(1);
+            printf("-----------\n");
+            sleep(1);
+            printf("|\n");
+            sleep(1);
+            printf("|\n");
+            printf("\033[0m");
+            
+            break;
+          case 'Y':
+          printf("\033[0;35m");
+           sleep(1);
+           printf("\\        /\n");
+           sleep(1);
+           printf(" \\      /\n");
+           sleep(1);
+           printf("  \\    /\n");
+           sleep(1);
+           printf("     |    \n");
+           sleep(1);
+           printf("     |    \n");
+           sleep(1);
+           printf("     |    \n");
+           printf("\033[0m");
+           
+           break;
+          case 'O':
+           printf("\033[0;32m");
+           sleep(1);
+           printf("----------\n");
+           sleep(1);
+           printf("|        |\n");
+           sleep(1);
+           printf("|        |\n");
+           sleep(1);
+           printf("|        |\n");
+           sleep(1);
+           printf("----------\n");
+           sleep(1);
+           printf("\033[0m");
+           
+           break;
+          case 'L':
+           printf("\033[0;34m");
+           sleep(1);
+           printf("|\n");
+           sleep(1);
+           printf("|\n");
+           sleep(1);
+           printf("|\n");
+           sleep(1);
+           printf("|\n");
+           sleep(1);
+           printf("---------\n");
+           sleep(1);
+           printf("\033[0m");
+           
+           break;
+         case 'I':
+           printf("\033[0;31m");
+           sleep(1);
+           printf("     |     \n");
+           sleep(1);
+           printf("     |     \n");
+           sleep(1);
+           printf("     |     \n");
+           sleep(1);
+           printf("     |     \n");
+           sleep(1);
+           printf("     |     \n");
+           printf("\033[0m");
+           
+           break;
+        default:
+         sleep(1);
+         printf("keyword not found");
+         printf("\e[1;1H\e[2J");
+         break;
+       }
+   }
+   return 0;
+}
