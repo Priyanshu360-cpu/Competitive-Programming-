5a269bfca44a87b868139620a5259b61a6f2bb40
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-03-25 14:45:50 +0530

    Update Leetcode - movezeros.cpp

--- Cpp/Leetcode - movezeros.cpp
+++ Cpp/Leetcode - movezeros.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int c=0;
-        vector<int>tr=nums;
-        int z=nums.size();
-         sort(tr.begin(), tr.end(), greater<int>());
-        if(tr[0]!=0){
-       for(int i=0;i<z;i++){
+int n=nums.size();
+        while(n){
+       for(int i=0;i<nums.size();i++){
            if(nums[i]==0){
-               for(int j=i;j<z-1;j++) nums[j]=nums[j+1];
-               i=i-1;
-           c=c+1;
+               for(int j=i;j<nums.size()-1;j++) nums[j]=nums[j+1];
+           nums.pop_back();
+           nums.push_back(0);
            }
-       }
-        for(int i=0;i<c;i++){
-            nums.pop_back();
-        }
-         for(int i=0;i<c;i++){
-            nums.push_back(0);
-        }
-    }
+       } 
+    n=n-1;}
     }
 };
