64d41cc959e0ac25ca8b2c27787b066cecfb660c
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-03-28 19:29:39 +0530

    Update Leetcode reverseString o(1).cpp

--- Cpp/Leetcode reverseString o(1).cpp
+++ Cpp/Leetcode reverseString o(1).cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int b=s.size()-1;
-        for(int i=0;i<(b+1)/2;i++){
-            char k=s[i];
-            s[i]=s[b];
-            s[b]=k;
-            b=b-1;
-        }
+       reverse(s.begin(),s.end());
     }
 };
