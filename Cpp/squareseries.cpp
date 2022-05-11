1f03004cdd09b6fa44afef21374685656a5f8a80
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-05-11 22:50:35 +0530

    Update squareseries.cpp

--- Cpp/squareseries.cpp
+++ Cpp/squareseries.cpp
@@ -3,20 +3,72 @@ using namespace std;
 int main() {
     int x;
 cin>>x;
+int p=x;
 if(x%2!=0){
-	for(int i=1;i<(x/2)+2;i++){
+    int i=1;
+	while(p>=1){
 	    for(int j=1;j<=i;j++){
+	        if(j==x-1-((x/2)-1)) continue;
 	        cout<<j<<"\t";
 	    }
-	    for(int j=i+1;j<(x-i);j++){
+	    for(int j=p-2;j>0;j--){
 	        cout<<i<<"\t";
 	    }
 	    for(int j=i;j>0;j--){
-	        if(j==(x/2)+1) continue;
 	        cout<<j<<"\t";
 	    }
-	    cout<<"\n";
-	}
+	    cout<<endl;
+	 p=p-2;
+	 i=i+1;
+}
+p=p+4;
+while(p!=x+2){
+    for(int j=1;j<i-2;j++){
+        cout<<j<<"\t";
+    }
+    for(int j=p-1;j>0;j--){
+        cout<<i-2<<"\t";
+    }
+    	    for(int j=i-2;j>0;j--){
+	        cout<<j<<"\t";
+	    }
+ p=p+2;
+ i=i-1;
+ cout<<endl;
+}
+
+}else{
+        int i=1;
+	while(p>=1){
+	    for(int j=1;j<=i;j++){
+	       
+	        cout<<j<<"\t";
+	    }
+	    for(int j=p-2;j>0;j--){
+	        cout<<i<<"\t";
+	    }
+	    for(int j=i;j>0;j--){
+	        cout<<j<<"\t";
+	    }
+	    cout<<endl;
+	 p=p-2;
+	 i=i+1;
+}
+p=p+4;
+while(p!=x+2){
+    for(int j=1;j<i-2;j++){
+        cout<<j<<"\t";
+    }
+    for(int j=p-1;j>0;j--){
+        cout<<i-2<<"\t";
+    }
+    	    for(int j=i-2;j>0;j--){
+	        cout<<j<<"\t";
+	    }
+ p=p+2;
+ i=i-1;
+ cout<<endl;
+}
 }
 	return 0;
 }
