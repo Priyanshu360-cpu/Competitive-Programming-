f97661ff19aa49865377cdc281c0bdc396386b2a
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-05-11 22:05:22 +0530

    Update squareseries.cpp

--- Cpp/squareseries.cpp
+++ Cpp/squareseries.cpp
@@ -3,17 +3,20 @@ using namespace std;
 int main() {
     int x;
 cin>>x;
-	for(int i=1;i<x+1;i++){
+if(x%2!=0){
+	for(int i=1;i<(x/2)+2;i++){
 	    for(int j=1;j<=i;j++){
-	        cout<<j;
+	        cout<<j<<"\t";
 	    }
 	    for(int j=i+1;j<(x-i);j++){
-	        cout<<i;
+	        cout<<i<<"\t";
 	    }
 	    for(int j=i;j>0;j--){
-	        cout<<j;
+	        if(j==(x/2)+1) continue;
+	        cout<<j<<"\t";
 	    }
 	    cout<<"\n";
 	}
+}
 	return 0;
 }
