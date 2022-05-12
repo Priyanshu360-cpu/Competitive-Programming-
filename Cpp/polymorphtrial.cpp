5c2f53f9665450a6e582a421e18d4b54bd55f9f2
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-05-12 20:02:01 +0530

    Create polymorphtrial.cpp

---
+++ Cpp/polymorphtrial.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class apple{
+    public:
+    string aalla;
+    void mapple();
+    apple(string b){
+        aalla = b;
+        cout<<aalla;
+        cout<<"no";
+    }
+};
+class mango: public apple{
+    public:
+    void pine(){
+        cout<<"no";
+    }
+}
+void apple::mapple(){
+        cout<<"hi";
+    }
+int main() {
+	  ap("letter");
+	 ap.mapple();
+	return 0;
+}
