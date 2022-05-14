634329c80b390e1839bb31a90564ab221eedae7b
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-05-14 19:29:31 +0530

    Update Leetcode - valid parantheses.cpp

--- Cpp/Leetcode - valid parantheses.cpp
+++ Cpp/Leetcode - valid parantheses.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     bool isValid(string s) {
         int b=0,c=0;
-        char d='a';
         vector <char> l;//store things in vector then pop back
         for(int i=0;i<s.size();i++){
             if(s[i]=='{'||s[i]=='('||s[i]=='['){
-                d=s[i];
+                d.push_back(s[i]);
             }
             if((s[i]=='}'||s[i]==')'||s[i]==']')&&(d=='a')) return false;
              if(s[i]=='}'||s[i]==')'||s[i]==']'){
