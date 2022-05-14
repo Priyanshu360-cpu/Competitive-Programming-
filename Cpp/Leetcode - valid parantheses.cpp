d7b58d7569821ad7755dbe4d180bf45778eef0ae
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-05-14 19:28:31 +0530

    Update Leetcode - valid parantheses.cpp

--- Cpp/Leetcode - valid parantheses.cpp
+++ Cpp/Leetcode - valid parantheses.cpp
@@ -3,6 +3,7 @@ public:
     bool isValid(string s) {
         int b=0,c=0;
         char d='a';
+        vector <char> l;//store things in vector then pop back
         for(int i=0;i<s.size();i++){
             if(s[i]=='{'||s[i]=='('||s[i]=='['){
                 d=s[i];
