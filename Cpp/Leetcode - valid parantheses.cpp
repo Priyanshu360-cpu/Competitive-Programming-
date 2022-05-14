c3ca38ffed7fc0e89ac29158c362bd22dc961ccd
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-05-14 20:23:15 +0530

    Update Leetcode - valid parantheses.cpp

--- Cpp/Leetcode - valid parantheses.cpp
+++ Cpp/Leetcode - valid parantheses.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     bool isValid(string s) {
         int b=0,c=0;
-        vector <char> l;//store things in vector then pop back
+        vector <char> d;
+        d.push_back('a');
         for(int i=0;i<s.size();i++){
             if(s[i]=='{'||s[i]=='('||s[i]=='['){
                 d.push_back(s[i]);
             }
-            if((s[i]=='}'||s[i]==')'||s[i]==']')&&(d=='a')) return false;
+            if((s[i]=='}'||s[i]==')'||s[i]==']')&&(d[d.size()-1]=='a')) return false;
              if(s[i]=='}'||s[i]==')'||s[i]==']'){
-               if((d=='{')&&(s[i]!='}')){
+               if((d[d.size()-1]=='{')&&(s[i]!='}')){
                    return false;
                }
-               if((d=='[')&&(s[i]!=']')){
+               if((d[d.size()-1]=='[')&&(s[i]!=']')){
                    return false;
                }
-                 if((d=='(')&&(s[i]!=')')){
+                 if((d[d.size()-1]=='(')&&(s[i]!=')')){
                    return false;
                }
+                 d.pop_back();
              }
 }
         return true;
