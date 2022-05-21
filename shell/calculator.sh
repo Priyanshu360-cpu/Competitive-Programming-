b934e53ade247b3820842fdb8e98c63c5f2614e8
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-05-21 14:30:44 +0530

    Update calculator.sh

--- shell/calculator.sh
+++ shell/calculator.sh
@@ -4,12 +4,25 @@ echo "Enter First Data"
 read data1
 echo "Enter Second Data"
 read data2
-echo "Press 1 for addition\nPress 2 for Subtraction\nPress 3 for Multiplication\nPress 4 for Division"
+printf "Press 1 for addition\nPress 2 for Subtraction\nPress 3 for Multiplication\nPress 4 for Division\n"
 read data3;
 if [ $data3 -eq 1 ]
 then
  val=`expr $data1 + $data2`
  echo "$val"
-else
- echo "\n"
+fi
+if [ $data3 -eq 2 ]
+then
+ val=`expr $data1 - $data2`
+ echo "$val"
+fi
+if [ $data3 -eq 3 ]
+then
+ val=`expr $data1 \* $data2`
+ echo "$val"
+fi
+if [ $data3 -eq 4 ]
+then
+ val=`expr $data1 / $data2`
+ echo "$val"
 fi
