aa33bf66153e752bca1518400d64f8fdcc5628da
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-05-31 12:23:43 +0530

    Create primepointerarray.c

---
+++ C/primepointerarray.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int* prime(int x,int y,int *m){
+    static int l=0;
+    for(int i=x;i<y;i++){
+        int c=0;
+        for(int j=1;j<x;j++){
+          if(i%j==0){c=c+1;}  
+        }
+        if(c<=2){l=l+1;}
+    }
+    *m=&l;
+    int p=0;
+    
+    static int a[100];
+    for(int i=x;i<y;i++){
+        int c=0;
+        for(int j=1;j<x;j++){
+          if(i%j==0) c=c+1;  
+        }
+        if(c<2){
+            a[p]=i;
+            p=p+1;
+        };
+    }
+    return a;
+}
+int main(void) {
+    int *o;
+    int *j;
+    j=prime(15,25,&o);
+ for(int i=0;i<*o;i++){
+     printf("%d\n",*(j+i));
+ }
+	return 0;
+}
+
