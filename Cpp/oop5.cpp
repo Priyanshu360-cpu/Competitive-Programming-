be5be7b04267325b91e4369e56ceb83ee67803a0
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-03 22:17:54 +0530

    Create oop5.cpp

---
+++ Cpp/oop5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+    class abc{
+    public:
+    int a,b,c=3;
+    public:
+    void output();
+    abc();
+    void display(int x,int y){
+        a=x,b=y;
+    }
+    void display(){
+        cout<<a<<"\n"<<b;
+    }
+};
+ abc::abc(){
+        cout<<"I am abc\n"<<this->c<<"\n";
+    };
+    void abc::output(){
+        cout<<"here"<<endl;
+    }
+int main() {
+class cde:public abc{
+    public:
+    cde(){
+        cout<<"i am cde \n";
+        this->c=5;
+        cout<<c<<"\n";
+        cout<<this<<endl;
+    }
+};
+ cde n;
+ n.display(4,5);
+ n.display();
+	return 0;
+}
