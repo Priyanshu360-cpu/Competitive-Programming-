53b0f49a9c133031b4182fac0193abec12385ce5
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-04 14:56:04 +0530

    Update LeetCode - candy.cpp

--- Cpp/LeetCode - candy.cpp
+++ Cpp/LeetCode - candy.cpp
@@ -12,15 +12,12 @@ public:
             }else{
                 if(ratings[i]>ratings[i+1])check[i]=check[i]+check[i+1];
                     if(ratings[i]>ratings[i-1])check[i]=check[i]+check[i-1];
-            }
-            
+            }   
         }
-  
       if(ratings[0]>ratings[1])
      {check[0]=check[1]+check[0];
       cout<<check[0];} if(ratings[ratings.size()-1]>ratings[ratings.size()-2])check[check.size()-1]=check[check.size()-1]+check[check.size()-2];
-        if(ratings[ratings.size()-1]<ratings[ratings.size()-2])check[check.size()-1]=check[check.size()-1]-2;
-          
+        else if(ratings[ratings.size()-1]<ratings[ratings.size()-2])check[check.size()-2]=check[check.size()-2]-1;
         int c=0;
         for(int i=0;i<check.size();i++) c=c+check[i];
         return c;
