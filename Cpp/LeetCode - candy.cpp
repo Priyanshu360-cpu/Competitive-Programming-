05e0c61fe733c711aabed5cdc1e30a2dd16090d4
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-04 15:40:50 +0530

    Update LeetCode - candy.cpp

--- Cpp/LeetCode - candy.cpp
+++ Cpp/LeetCode - candy.cpp
@@ -6,20 +6,19 @@ public:
         for(int i=0;i<ratings.size();i++){
             check.push_back(1);
         }
-        
-        for(int i=0;i<check.size()-1;i++){
+        if(ratings[ratings.size()-1]<ratings[ratings.size()-2])check[check.size()-2]=check[check.size()-2]+1;
+        for(int i=0;i<check.size()-2;i++){
             if(i==0){
                 continue;
             }else{
                 if(ratings[i]>ratings[i+1])check[i]=check[i]+check[i+1];
-                    if(ratings[i]>ratings[i-1])check[i]=check[i]+check[i-1];
-                    if(ratings[i]==ratings[i-1]&&ratings[i]==ratings[i+1]) check[i]=check[i-1];
+                else if(ratings[i]>ratings[i-1])check[i]=check[i]+check[i-1];
             }   
         }
       if(ratings[0]>ratings[1])
      {check[0]=check[1]+check[0];
-      cout<<check[0];} if(ratings[ratings.size()-1]>ratings[ratings.size()-2])check[check.size()-1]=check[check.size()-1]+check[check.size()-2];
-        else if(ratings[ratings.size()-1]<ratings[ratings.size()-2])check[check.size()-2]=check[check.size()-2]-1;
+      cout<<check[0];} if(ratings[ratings.size()-1]>=ratings[ratings.size()-2])check[check.size()-1]=check[check.size()-1]+check[check.size()-2];
+         
         int c=0;
         for(int i=0;i<check.size();i++) c=c+check[i];
         return c;
