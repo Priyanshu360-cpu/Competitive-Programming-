88795bd786ee6a83da524380efe09a717daeb7c3
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-04 14:48:56 +0530

    Create LeetCode - candy.cpp

---
+++ Cpp/LeetCode - candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+      vector<int> check;
+        if(ratings.size()==1) return 1;
+        for(int i=0;i<ratings.size();i++){
+            check.push_back(1);
+        }
+        for(int i=0;i<check.size()-1;i++){
+            if(i==0){
+                continue;
+            }else{
+                if(ratings[i]>ratings[i+1])check[i]=check[i]+check[i+1];
+                    if(ratings[i]>ratings[i-1])check[i]=check[i]+check[i-1];
+            }
+            
+        }
+  
+      if(ratings[0]>ratings[1])
+     {check[0]=check[1]+check[0];
+      cout<<check[0];} if(ratings[ratings.size()-1]>ratings[ratings.size()-2])check[check.size()-1]=check[check.size()-1]+check[check.size()-2];
+        if(ratings[ratings.size()-1]<ratings[ratings.size()-2])check[check.size()-1]=check[check.size()-1]-2;
+          
+        int c=0;
+        for(int i=0;i<check.size();i++) c=c+check[i];
+        return c;
+    }
+};
