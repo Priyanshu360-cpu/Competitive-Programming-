d2dbfc4528e3dbe646cd4a1d9641003118c11429
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-06 00:38:41 +0530

    Update LeetCode - longest-consecutive-sequence.cpp

--- Cpp/LeetCode - longest-consecutive-sequence.cpp
+++ Cpp/LeetCode - longest-consecutive-sequence.cpp
@@ -2,29 +2,24 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         if(!nums.size()) return 0;
-        
         vector<int> diff;
         vector<int> unique;
         diff.push_back(1);
         int c=1;
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
-            int l=0;
-for(int j=0;j<unique.size();j++){
-    if(unique[j]==nums[i]){
-    l=1;
-    break;
-}}
-    if(l==0){
-      unique.push_back(nums[i]);
-            }
-        }
-  
-        for(int i=0;i<unique.size()-1;i++){
-            if(unique[i+1]-unique[i]==1) {
+             int l=0;
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[j]==nums[i]){
+                    i=j; l==3;continue;
+                }
+                if(nums[j]==nums[i]+1){
+                    l=1;break;
+                }
+                  if(nums[j]>nums[i])break;}
+            if(l==1){
                 c=c+1;
-               }
-            else{
+            }else if(l==3) continue; else{
                 diff.push_back(c);
                 c=1;
             }
