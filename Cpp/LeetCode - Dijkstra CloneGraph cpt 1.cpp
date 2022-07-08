e73051429f16d72f454e076ca883fbcc456c24e0
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-08 14:14:23 +0530

    Update LeetCode - Dijkstra CloneGraph cpt 1.cpp

--- Cpp/LeetCode - Dijkstra CloneGraph cpt 1.cpp
+++ Cpp/LeetCode - Dijkstra CloneGraph cpt 1.cpp
@@ -17,13 +17,33 @@ public:
     }
 };
 
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
 
 class Solution {
 public:
     void check(queue<int> &s,Node* node, vector<int> &visited){
 int h=s.front();
         s.pop();
-        fot(int i=0;i<visited.size();i++)
+        for(int i=0;i<visited.size();i++)
         if(visited[i]==h) cout<<"step"<<endl;
     }
     Node* cloneGraph(Node* node) {
@@ -35,7 +55,7 @@ int h=s.front();
        for(int i=0;i<node->neighbors.size();i++) cout<<node->neighbors[i]->neighbors[1]->
            neighbors[1]->val<<endl;
        for(int i=0;i<<node->neighbors[1]->neighbors.size();i++) cout<<node->neighbors[1]->neighbors[i]->val<<endl;
-        
+        check(p,node,visited);
         return l;
     }
 };
