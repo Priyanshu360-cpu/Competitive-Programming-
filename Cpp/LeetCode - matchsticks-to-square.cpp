434081295d3b9283fff4172502472c8f1ad4dc40
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-12 17:11:45 +0530

    Create LeetCode - matchsticks-to-square.cpp

---
+++ Cpp/LeetCode - matchsticks-to-square.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool makesquare(vector<int>& matchsticks) {
+        sort(matchsticks.begin(),matchsticks.end());
+        if((accumulate(matchsticks.begin(),matchsticks.end(),0))%4!=0)return false;
+        int a=accumulate(matchsticks.begin(),matchsticks.end(),0)/4;
+        cout<<a;
+        for(int i=0;i<matchsticks.size()-1;i++)
+        {
+            if(matchsticks[i]==0) continue;
+            else{
+                int k=0;
+                for(int j=0;j<matchsticks.size();j++){
+                    if(j==i)continue;
+                    if(matchsticks[i]+matchsticks[j]==a){
+                        matchsticks[i]=0;
+                        matchsticks[j]=0;
+                        k=1;
+                        break;
+                    }
+                    int m=j;
+                    int l;
+                    if(matchsticks[i]+matchsticks[j]<a){
+                        if(matchsticks[i]==matchsticks[j]){
+                            l=0;
+                        } else
+                         l=matchsticks[j];
+                         matchsticks[i]=matchsticks[i]+matchsticks[j];
+                        while(j<matchsticks.size()&&matchsticks[i]<a){
+                            if(matchsticks[j]!=l)
+                            {matchsticks[i]=matchsticks[i]+matchsticks[j];
+                              cout<<matchsticks[i];
+                             l=matchsticks[j];
+                             matchsticks[j]=0;
+                            }
+                            j=j+1;
+                        }
+                        if(matchsticks[i]==a) cout<<matchsticks[i];
+                        if(matchsticks[i]!=a) return false;
+                        else {
+                            j=m;
+                            matchsticks[i]=0;
+                        }
+                    }
+                }
+                if(k==0) { return false;}
+            }
+        }
+    return true;}
+};
