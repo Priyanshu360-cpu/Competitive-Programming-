8859d076754ee219baadcf2cd46a24f5685abf69
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-15 14:11:21 +0530

    Update LeetCode - max-area-of-island.cpp

--- Cpp/LeetCode - max-area-of-island.cpp
+++ Cpp/LeetCode - max-area-of-island.cpp
@@ -1,60 +1,123 @@
 class Solution {
 public:
-    void right(vector<vector<int>>& a,int j,int i,int k,int m,int* p){
-        
+    void right(vector<vector<int>>& a,vector<int>& h,int j,int i,int k,int m,int* p){
+        vector<int>::iterator it;
+            it = find (h.begin(), h.end(), ((i*10)+j));
+                           
+        if(it==h.end()){
 
-        if(j+1<k){
-            if(a[i][j+1]==1){
-                *p=*p+1;
-                right(a,j+1,i,k,m,p);
-            }
+  h.push_back((i*10)+j);
+        *p=*p+1;
+          if(i+1<m){
+            if(a[i+1][j]==1) down(a,h,j,i+1,k,m,p);
         }
+        if(j+1<k){
+            if(a[i][j+1]==1) right(a,h,j+1,i,k,m,p);
+        }
+                if(i-1>=0){
+            if(a[i-1][j]==1) up(a,h,j,i-1,k,m,p);
+        }
+                 if(j-1>=0){
+            if(a[i][j-1]==1) left(a,h,j-1,i,k,m,p);
+        }
+        }
+        
+    }
+        void down(vector<vector<int>>& a,vector<int>& h,int j,int i,int k,int m,int* p){
+             vector<int>::iterator it;
+            it = find (h.begin(), h.end(), (i*10)+j);
+            if(it==h.end()){
+  h.push_back((i*10)+j);
+        *p=*p+1;
+         if(i+1<m){
+            if(a[i+1][j]==1) down(a,h,j,i+1,k,m,p);
+        }
+        if(j+1<k){
+            if(a[i][j+1]==1) right(a,h,j+1,i,k,m,p);
+        }
+                if(i-1>=0){
+            if(a[i-1][j]==1) up(a,h,j,i-1,k,m,p);
+        }
+                 if(j-1>=0){
+            if(a[i][j-1]==1) left(a,h,j-1,i,k,m,p);
+        }
+        
+            }
+            
+    }
+    void up(vector<vector<int>>& a,vector<int>& h,int j,int i,int k,int m,int* p){
+             vector<int>::iterator it;
+            it = find (h.begin(), h.end(), (i*10)+j);
+            if(it==h.end()){
+  h.push_back((i*10)+j);
+        *p=*p+1;
+        
         if(i+1<m){
-            if(a[i+1][j]==1){
-                *p=*p+1;
-                down(a,j,i+1,k,m,p);
-            }
-        }
-        
-    }
-        void down(vector<vector<int>>& a,int j,int i,int k,int m,int* p){
-        
-              if(i+1<m){
-            if(a[i+1][j]==1){
-                *p=*p+1;
-                down(a,j,i+1,k,m,p);
-            }
+            if(a[i+1][j]==1) down(a,h,j,i+1,k,m,p);
         }
         if(j+1<k){
-            if(a[i][j+1]==1){
-                *p=*p+1;
-                right(a,j+1,i,k,m,p);
-            }
+            if(a[i][j+1]==1) right(a,h,j+1,i,k,m,p);
         }
-      
+                if(i-1>=0){
+            if(a[i-1][j]==1) up(a,h,j,i-1,k,m,p);
+        }
+                 if(j-1>=0){
+            if(a[i][j-1]==1) left(a,h,j-1,i,k,m,p);
+        }
         
-        
+            }
+            
     }
+     void left(vector<vector<int>>& a,vector<int>& h,int j,int i,int k,int m,int* p){
+             vector<int>::iterator it;
+            it = find (h.begin(), h.end(), (i*10)+j);
+            if(it==h.end()){
+  h.push_back((i*10)+j);
+        *p=*p+1;
+        
+        if(i+1<m){
+            if(a[i+1][j]==1) down(a,h,j,i+1,k,m,p);
+        }
+        if(j+1<k){
+            if(a[i][j+1]==1) right(a,h,j+1,i,k,m,p);
+        }
+                if(i-1>=0){
+            if(a[i-1][j]==1) up(a,h,j,i-1,k,m,p);
+        }
+                 if(j-1>=0){
+            if(a[i][j-1]==1) left(a,h,j-1,i,k,m,p);
+        }
+        
+            }
+            
+    }
+    
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         vector<vector<int>> a=grid;
         vector<int> b;
         for(int i=0;i<a.size();i++){
             for(int j=0;j<a[i].size();j++){
+                vector<int> h;
                 int p=0;
                 if(a[i][j]==1){
+                    p=p+1;
+                    h.push_back(((i*10)+j));
                     if(j+1<a[i].size()&&a[i][j+1]==1){
-                        p=p+1;
-                        right(a,j+1,i,a[i].size(),a.size(),&p);
+                        right(a,h,j+1,i,a[i].size(),a.size(),&p);
                     }if(i+1<a.size()&&a[i+1][j]==1){
-                        p=p+1;
-                        down(a,j,i+1,a[i].size(),a.size(),&p);
+                        down(a,h,j,i+1,a[i].size(),a.size(),&p);
                     }
+                    if(i-1>=0){
+            if(a[i-1][j]==1) up(a,h,j,i-1,a[i].size(),a.size(),&p);
+        }
+                 if(j-1>=0){
+            if(a[i][j-1]==1) left(a,h,j-1,i,a[i].size(),a.size(),&p);
+        }
                 }
                 b.push_back(p);
             }
         }
         sort(b.begin(),b.end());
-        
-        return(b[b.size()-1]);
+        return (b[b.size()-1]);
     }
 };
