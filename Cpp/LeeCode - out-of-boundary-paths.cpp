b3245de14e0233a3c6d1da5577a27896e403a493
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-16 16:17:54 +0530

    Update LeeCode - out-of-boundary-paths.cpp

--- Cpp/LeeCode - out-of-boundary-paths.cpp
+++ Cpp/LeeCode - out-of-boundary-paths.cpp
@@ -3,22 +3,22 @@ public:
     
     void solve(int m,int n,int sr,int sc,int *p,int g){
         if(*p==2147483647){
-            return ;
+            return;
         }
             if(sr<0&&sc>=0){
-                if(g==0)*p=*p+1;
+                if(g==0){*p=*p+1; return;}
                 else return;
             }
             if(sc<0&&sr>=0){
-                if(g==0)*p=*p+1;
+                if(g==0){*p=*p+1; return;}
                 else return;
             }
             if(sr==m&&sc<n){
-                if(g==0)*p=*p+1;
+                if(g==0){*p=*p+1; return;}
                 else return;
             }
           if(sr<m&&sc==n){
-                if(g==0) *p=*p+1;
+                if(g==0) {*p=*p+1; return;}
                 else return;
             }
         if(g!=0){
@@ -33,7 +33,6 @@ public:
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
          int p=0;
          int h=1;
-
         while(h<=maxMove){
             solve(m,n,startRow,startColumn,&p,h);
             h=h+1;
