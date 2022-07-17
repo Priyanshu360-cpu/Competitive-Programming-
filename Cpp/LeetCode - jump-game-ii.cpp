07e2e2650a5b480c044a5fb2d0c4f273bf8d3e08
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-17 20:56:31 +0530

    Update LeetCode - jump-game-ii.cpp

--- Cpp/LeetCode - jump-game-ii.cpp
+++ Cpp/LeetCode - jump-game-ii.cpp
@@ -6,12 +6,21 @@ public:
         if(nums.size()==1) return 0;
         for(int i=0;i<nums.size();i++){
   int m=nums[i];
-            if(m+i<nums.size()-1){
-            sort(nums.begin()+i+1,nums.begin()+m+i+1);
-            i=i+m;
+            cout<<i<<endl;
+            if(m+i<=nums.size()){
+                              
+vector<int>a;
+                vector<int> b;
+                a.assign(nums.begin()+i+1,nums.begin()+m+i+1);
+                b=a;
+                sort(a.begin(),a.end());
+                vector<int>::iterator it;
+                it=find(b.begin(),b.end(),a[a.size()-1]);
+                
+            i=i+a[a.size()-1]+(it-b.begin());
             p=p+1;
             }
-            }
+        }
         return p;
         }
 };
