1117d66c8225f4c11715107ddf0143f96c90cc0a
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-19 15:58:24 +0530

    Update and rename LeetCode - pascals-triangle.cpp to LeetCode - pascals-triangle1&2.cpp

--- Cpp/LeetCode - pascals-triangle1&2.cpp
+++ Cpp/LeetCode - pascals-triangle1&2.cpp
@@ -19,4 +19,21 @@ public:
         finale(a,numRows,1);
         return a;
     }
+    //question2 starts here
+     void finale2(vector<int>& a,int g,int p){
+        if(p==g) return;
+        vector<int> b;
+        b.push_back(1);
+        for(int i=0;i<a.size()-1;i++) b.push_back(a[i]+a[i+1]);
+        b.push_back(1);
+        a=b;
+        finale2(a,g,p+1);
+    }
+    vector<int> getRow(int rowIndex) {
+        vector<int> a;
+        a.push_back(1);
+        if(rowIndex+1==1) return a;
+        finale2(a,rowIndex+1,1);
+        return a;
+    }
 };
