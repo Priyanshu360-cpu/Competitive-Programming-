cefed2b08c3b8097d6e5d978d9ec328736d2ac7b
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-19 11:45:39 +0530

    Update LeetCode - group-anagrams.cpp

--- Cpp/LeetCode - group-anagrams.cpp
+++ Cpp/LeetCode - group-anagrams.cpp
@@ -4,25 +4,15 @@ public:
        map<string,vector<string>> a;
         for(int i=0;i<strs.size();i++){
             string gh=strs[i];
-            int p=0;
+            sort(strs[i].begin(),strs[i].end());
           if(a.find(strs[i])!=a.end()){
               a[strs[i]].push_back(gh);
-          }else{
-            next_permutation(strs[i].begin(),strs[i].end());
-            while(strs[i]!=gh){
-                if(a.find(strs[i])!=a.end()){
-                    a[strs[i]].push_back(gh);
-                    p=1;
-                    break;
-                }
-                next_permutation(strs[i].begin(),strs[i].end());
-            }
-            if(p==0){
+          }else{   
                 vector<string> b;
                 b.push_back(gh);
-                a.insert(pair<string,vector<string>>(gh,b));
+                a.insert(pair<string,vector<string>>(strs[i],b));
             }
-        }}
+        }
         vector<vector<string>> c;
         for (auto i : a){
             c.push_back(i.second);
