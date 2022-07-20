d4076d49e62bba9f63e34abeecba72822d0a2d8b
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-20 13:44:59 +0530

    Create LeetCode - number-of-matching-subsequences.cpp

---
+++ Cpp/LeetCode - number-of-matching-subsequences.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        string::iterator it;
+        int p=0;
+        for(auto x:words){
+            vector<int> z;
+            if(x.size()==1){
+                it=find(s.begin(),s.end(),x[0]);
+                if(it!=s.end()){
+                    p=p+1;
+                    continue;
+                }
+            }
+            for(int i=0;i<s.size();i++){
+                if(x[0]==s[i])z.push_back(i);
+            }
+            for(auto y:z){
+                int q=0;
+                vector<int> h;
+                for(auto o:x){
+                    it=find(s.begin()+y,s.end(),o);
+                    if(it!=s.end()){
+                        h.push_back(it-s.begin()+y);
+                    }else{
+                        q=1;
+                        break;
+                    }
+                }
+                if(q==0){
+                    int g=0;
+                     for(int i=0;i<h.size()-1;i++){
+                if(h[i]>=h[i+1]){
+                    g=1;
+                    break;
+                }
+            }
+            if(g==0) p=p+1;
+                }
+            }
+        }
+        return p;
+    }
+};
