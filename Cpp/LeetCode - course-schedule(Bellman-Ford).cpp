5f4e0f284e6b8ddabe467ac39d0881530694f329
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-21 01:26:20 +0530

    Update LeetCode - course-schedule(Bellman-Ford).cpp

--- Cpp/LeetCode - course-schedule(Bellman-Ford).cpp
+++ Cpp/LeetCode - course-schedule(Bellman-Ford).cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
+    void dfs(vector<int> b,vector<bool>& s,vector<int> a[],int i,int V){
+        if(i>=V){
+           s.push_back(true);
+            return;
+        }
+      for(auto x: a[i]){
+          vector<int>::iterator it;
+          it=find(b.begin(),b.end(),x);
+          if(it!=b.end()) s.push_back(false);
+          else{
+              for(auto g:b) cout<<g<<endl;
+              b.push_back(x);
+              dfs(b,s,a,x,V);
+              b.pop_back();
+          }
+      }
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         if(prerequisites.size()==0) return true;
         vector<int> a[numCourses];
-        for(int i=0;i<prerequisites.size();i++)
+        for(int i=0;i<prerequisites.size();i++){
             for(int j=1;j<prerequisites[i].size();j++)a[prerequisites[i][0]].push_back(prerequisites[i][j]);
-        int p;
-        for(int i=0;i<numCourses;i++)if(a[i].size()!=0){p=i;
-        queue<int> b;
-        vector<int>::iterator it;
-        vector<int> c;
-        b.push(p);
-        while(!b.empty()){
-            c.push_back(b.front());
-            for(auto x:a[b.front()]){
-                it=find(c.begin(),c.end(),x);
-                if(it!=c.end()&&x<=b.front()){
-                    cout<<x<<b.front();
-                    return false;}
-                else{
-                    b.push(x);
-                }
-            }
-            b.pop();
         }
-                                                       }
-        return true;
+        int p;
+        for(int i=0;i<numCourses;i++){
+            if(a[i].size()!=0){
+                p=i;
+                break;
+            }
+        }
+        vector<int> b;
+        vector<bool> s;
+        dfs(b,s,a,p,numCourses);         
+     for(auto x:s) if(x) return true;
+        return false;
     }
 };
