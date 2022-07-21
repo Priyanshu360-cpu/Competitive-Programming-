e2bda7cf4b514eba3339b608c6315083c7687bfc
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-21 13:18:39 +0530

    Update LeetCode - copy-list-with-random-pointer.cpp

--- Cpp/LeetCode - copy-list-with-random-pointer.cpp
+++ Cpp/LeetCode - copy-list-with-random-pointer.cpp
@@ -31,14 +31,10 @@ public:
                 it=find(a.begin(),a.end(),a[i]->random);
                 b[i]->random=b[it-a.begin()];
             }
-            
         }
-        for(int i=0;i<b.size()-1;i++){
-            b[i]->next=b[i+1];
-        }
+        for(int i=0;i<b.size()-1;i++)b[i]->next=b[i+1];
         b[b.size()-1]->next=NULL;
-        Node* e=b[0];
-        return e;
+        return b[0];
     }
 };
 
