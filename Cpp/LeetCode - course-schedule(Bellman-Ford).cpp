f5d46056495a2cccb9236434dbf2d049ac6b65f1
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-22 01:05:56 +0530

    Create LeetCode - course-schedule(Bellman-Ford).cpp

--- Cpp/LeetCode - course-schedule(Bellman-Ford).cpp
+++ Cpp/LeetCode - course-schedule(Bellman-Ford).cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    void dfs(vector<int> b,vector<bool>& s,vector<int> a[],int i,int V){
-        if(i>=V){
+    void dfs(vector<int> b,vector<bool>& s,vector<int> a[],int i,int j,int V,int* p){
+        if(*p==1) return;
+        if(a[i].size()==0) return;
+        if(j>=V){
            s.push_back(true);
             return;
         }
       for(auto x: a[i]){
           vector<int>::iterator it;
           it=find(b.begin(),b.end(),x);
-          if(it!=b.end()) s.push_back(false);
+          if(it!=b.end()){
+              *p=1;
+              s.push_back(false);
+              return;}
           else{
-              for(auto g:b) cout<<g<<endl;
               b.push_back(x);
-              dfs(b,s,a,x,V);
+              if(a[x].size()!=0)
+              dfs(b,s,a,x,j+1,V,p);
               b.pop_back();
           }
       }
@@ -23,17 +28,15 @@ public:
         for(int i=0;i<prerequisites.size();i++){
             for(int j=1;j<prerequisites[i].size();j++)a[prerequisites[i][0]].push_back(prerequisites[i][j]);
         }
-        int p;
-        for(int i=0;i<numCourses;i++){
-            if(a[i].size()!=0){
-                p=i;
-                break;
-            }
-        }
-        vector<int> b;
+        int p=0;
         vector<bool> s;
-        dfs(b,s,a,p,numCourses);         
-     for(auto x:s) if(x) return true;
-        return false;
+     for(int i=0;i<prerequisites.size();i++){
+         if(p==1) break;
+            vector<int> b;
+            b.push_back(prerequisites[i][0]);
+            dfs(b,s,a,prerequisites[i][0],i,numCourses,&p);    
+        }
+    if(s.size()&&s[s.size()-1]==false) return false;
+        return true;
     }
 };
