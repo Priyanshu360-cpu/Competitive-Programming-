6c48dfec315a93d56bb75dace8ec0974d85a7fa4
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-30 14:13:43 +0530

    Update LeetCode - word-subsets.cpp

--- Cpp/LeetCode - word-subsets.cpp
+++ Cpp/LeetCode - word-subsets.cpp
@@ -3,28 +3,22 @@ public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
        vector<string> a;
        for(int i=0;i<words1.size();i++){
-                  map<char,int> b;
-           for(int j=0;j<words1[i].size();j++)b[words1[i][j]]+=1;
+           string k=words1[i];
            int g=0;
-           for(int j=0;j<words2.size();j++){
-               map<char,int> c=b;
-               for(auto y:words2[j]){
-                if(c.find(y)==c.end()){
-                    g=1;
-                    break;
-                }else{
-                    if(c[y]<=0){
-                        g=1;
-                        break;
-                    }else{
-                        c[y]=c[y]-1;
-                    }
-                }
+      for(auto z:words2){
+          words1[i]=k;
+          for(auto x:z){
+          size_t found = words1[i].find(x);
+          if(found!= string::npos){
+            words1[i][found]='%';
+              }else{
+              g=1;
+              break;
+          }
        }
-               if(g==1)break;
-           }
-               if(g==0)a.push_back(words1[i]);
-           
+          if(g==1)break;
+      }
+           if(g==0)a.push_back(k);
            }
         return a;
     }
