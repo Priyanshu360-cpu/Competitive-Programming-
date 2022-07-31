d8ce9493793d58a4af4ab0a3c4f3bf6a26bb561e
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-07-31 11:21:11 +0530

    Update LeetCode - range-sum-query-mutable.cpp

--- Cpp/LeetCode - range-sum-query-mutable.cpp
+++ Cpp/LeetCode - range-sum-query-mutable.cpp
@@ -1,14 +1,23 @@
 class NumArray {
     public:
     vector<int> a;
+    int s=0;
 public:
     NumArray(vector<int>& nums) {
-         a=nums;
+        a=nums;
+        s=accumulate(a.begin(),a.end(),0);
     }
+    
     void update(int index, int val) {
+        s-=a[index];
        a[index]=val;
+        s+=a[index];
     }
+    
     int sumRange(int left, int right) {
-        return accumulate(a.begin()+left,a.begin()+right+1, 0);;
+        int v=s;
+        for(int i=0;i<left;i++)v-=a[i];
+        for(int i=right+1;i<a.size();i++)v-=a[i];
+        return v;
     }
 };
