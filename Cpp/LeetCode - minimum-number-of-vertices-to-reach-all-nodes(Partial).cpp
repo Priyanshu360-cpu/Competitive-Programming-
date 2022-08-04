c812fd79327ede1a5ac7e510f7daa5009d743707
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-08-04 15:16:41 +0530

    Update LeetCode - minimum-number-of-vertices-to-reach-all-nodes(Partial).cpp

--- Cpp/LeetCode - minimum-number-of-vertices-to-reach-all-nodes(Partial).cpp
+++ Cpp/LeetCode - minimum-number-of-vertices-to-reach-all-nodes(Partial).cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    void solution(){
-        l=0;
+    void solution(int n,map<int,vector<int>>& a,vector<bool>& b,vector<int>& c){
+        int l=0;
+        int max=0;
         for(int i=0;i<n;i++){
-            if(a[i]>max&&b[i]==true){
-                max=a[i];
+            if(a[i].size()>max&&b[i]==true){
+                max=a[i].size();
                 l=i;
             }
         }
-        traverse()
-        b[i]=false;
+        traverse(a[l],b,a);
+        b[l]=false;
+        c.push_back(l);
         for(auto x:b){
-            if(x==true)solution()
+            if(x==true)solution(n,a,b,c);
         }
     }
-    void traverse(){
+    void traverse(vector<int>& c,vector<bool>& b,map<int,vector<int>>& a){
         for(auto x:c){
             if(b[x]==true){
                 b[x]=false;
-                if(a.find(x)!=a.end(){
-                traverse(a[x])}
+                if(a.find(x)!=a.end()){
+                traverse(a[x],b,a);}
             }
         }
     }
@@ -29,8 +31,8 @@ public:
         for(int i=0;i<edges.size();i++){
             a[edges[i][0]].push_back(edges[i][1]);
         }
-        soltuion()
-        return edges[0];
-        
+        vector<int> c;
+        solution(n,a,b,c);
+        return c;
     }
 };
