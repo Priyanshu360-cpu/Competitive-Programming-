acef9241e6e348caececdb9aa4816f5a030d854f
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-08-21 14:13:02 +0530

    Update LeetCode - stamping-the-sequence.cpp

--- Cpp/LeetCode - stamping-the-sequence.cpp
+++ Cpp/LeetCode - stamping-the-sequence.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    void doit(vector<int>& a,string b,vector<int> c,int w,string target,string stamp,int* h,map<string,int>& q){
-        if(*h==1)return;
+    void doit(vector<vector<int>>& a,string b,vector<int> c,int w,string target,string stamp,map<string,int>& q){
+        cout<<b<<endl;
         if(w>=10*target.size())return;
         if(b==target){
-            a=c;
-            *h=1;
+            cout<<b<<"1"<<endl;
+            a.push_back(c);
             return;
         }else{
             for(int i=0;i<=target.size()-stamp.size();i++){
@@ -19,18 +19,19 @@ public:
                 if(q[b]==1)continue;
                 else q[b]=1;
                 c.push_back(i);
-            doit(a,b,c,w+1,target,stamp,h,q);
+            doit(a,b,c,w+1,target,stamp,q);
                 c.pop_back();
             b=d;
         }
         }
     }
     vector<int> movesToStamp(string stamp, string target) {
-        vector<int> a;
-        if(stamp.size()>target.size())return a;
+        vector<vector<int>> a;
+        vector<int> n;
+        if(stamp.size()>target.size())return n;
         else if(stamp.size()==target.size()){
-a.push_back(0);
-        return a;}
+n.push_back(0);
+        return n;}
         int h=0;
         map<string,int> q;
         string b="";
@@ -46,10 +47,11 @@ a.push_back(0);
             q[b]=1;
             vector<int> c;
             c.push_back(i);
-            doit(a,b,c,1,target,stamp,&h,q);
+            doit(a,b,c,1,target,stamp,q);
             b=d;
         }
-
-        return a;
+sort(a.begin(),a.end());
+        cout<<a.size();
+        return a[0];
     }
 };
