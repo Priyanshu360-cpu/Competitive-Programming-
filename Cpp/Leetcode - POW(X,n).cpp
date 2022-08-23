5474a114085a891f2e45f01ecdf5a80ff096e261
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-08-24 00:26:05 +0530

    Update Leetcode - POW(X,n).cpp

--- Cpp/Leetcode - POW(X,n).cpp
+++ Cpp/Leetcode - POW(X,n).cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n==0) return 1;
-        if(x==1) return 1;
-        int u=0;
-        double z=x;
-        if(n<0){
-            n=-n;
-            u=1;
-        }
-        for(int i=0;i<n-1;i++)x=x*z;
-        if(u==1) return 1/x;
-        else return x;
+ return x<0?(n%2==0?(exp(n*log(-x))):-exp(n*log(-x))):exp(n*log(x));
     }
 };
