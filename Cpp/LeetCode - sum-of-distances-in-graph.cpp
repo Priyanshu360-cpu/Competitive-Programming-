59748e8effca92c69afe51196530d2e61e6d2302
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-08-24 00:25:37 +0530

    Update LeetCode - sum-of-distances-in-graph.cpp

--- Cpp/LeetCode - sum-of-distances-in-graph.cpp
+++ Cpp/LeetCode - sum-of-distances-in-graph.cpp
@@ -13,24 +13,18 @@ for(auto x:a[k])b[x]=true;
 b[k]=true;
         int h=0;
         for(auto x:b){
-if(!x.second){
-    h+=fetcher(a,dp,x.first,1,a[k]);
-         if(k==0)cout<<h<<" "<<x.first<<endl;
-
-}else{
-    h+=1;
-}}
+if(!x.second)
+    h+=fetcher(a,dp,x.first,1,a[k]);else h+=1;}
   dp[k]=h-1;
     }
     int fetcher(map<int,vector<int>> a,vector<int>& dp,int key,int b,vector<int> c){
         if(b>=dp.size()-1)return dp.size()-1;
+         int k=dp.size()-1;
         for(auto x:c){
             if(x==key) {
-                int d=dp.size()-1;
-                return min(d,b);
+                return min(k,b);
             }
         }
-            int k=dp.size()-1;
             for(auto x:c) k=min(k,fetcher(a,dp,key,b+1,a[x]));
             return k;
     }
