5de791e5b152b3568f2e5d4a2ba51157a549a486
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-08-23 22:00:51 +0530

    Create LeetCode - sum-of-distances-in-graph.cpp

---
+++ Cpp/LeetCode - sum-of-distances-in-graph.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void solvable(map<int,vector<int>> a,vector<int>& dp,int k){
+        map<int,bool> b;
+
+for(int i=0;i<dp.size();i++)b[i]=false;
+for(auto x:a[k])b[x]=true;
+b[k]=true;
+        int h=0;
+        for(auto x:b){
+if(!x.second){
+    h+=fetcher(a,dp,x.first,1,a[k]);
+         if(k==0)cout<<h<<" "<<x.first<<endl;
+
+}else{
+    h+=1;
+}}
+  dp[k]=h-1;
+    }
+    int fetcher(map<int,vector<int>> a,vector<int>& dp,int key,int b,vector<int> c){
+        if(b>=dp.size()-1)return dp.size()-1;
+        for(auto x:c){
+            if(x==key) {
+                int d=dp.size()-1;
+                return min(d,b);
+            }
+        }
+            int k=dp.size()-1;
+            for(auto x:c) k=min(k,fetcher(a,dp,key,b+1,a[x]));
+            return k;
+    }
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        vector<int> dp(n,0);
+        map<int,vector<int>> a;
+        for(auto x:edges){
+            a[x[0]].push_back(x[1]);
+            a[x[1]].push_back(x[0]);
+        }
+        for(int i=0;i<n;i++)solvable(a,dp,i);
+        return dp;
+    }
+};
