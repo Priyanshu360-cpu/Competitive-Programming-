34a2d6dd8b9d2e9071d0cbe43f5235364c390e60
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-08-26 22:40:11 +0530

    Update LeetCode - reordered-power-of-2.cpp

--- Cpp/LeetCode - reordered-power-of-2.cpp
+++ Cpp/LeetCode - reordered-power-of-2.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
-    bool tryit(vector<int>& a,vector<int> b){
-        if(a==b) return false;
-        int x=0;
-        if(a[0]==0)goto L;
-        for(int i=0;i<a.size();i++){
-            x=x*10+a[i];
-        }
-        if(pow(2,floor(log(x)/log(2)))==x)return true;
-        else{
-            L:
-             int k=a[0];
-        for(int i=0;i<a.size()-1;i++){
-            a[i]=a[i+1];
-        }
-            
-        a[a.size()-1]=k;
-            return false||tryit(a,b);
-        }
-    }
+
     bool reorderedPowerOf2(int n) {
         vector<int> a;
         int x=n;
@@ -26,14 +8,25 @@ public:
         while(x){
             a.push_back(x%10);
             x/=10;};
-        vector<int> b=a;
-        int k=a[0];
-        for(int i=0;i<a.size()-1;i++){
-                cout<<a[i];
-            a[i]=a[i+1];
+        int o=0;
+        if(a[0]!=0){
+        for(auto x:a)o=o*10+x;
+            if(pow(2,floor(log(o)/log(2)))==o) return true;
         }
-        a[a.size()-1]=k;
-        
-        return n==1?true:(false||tryit(a,b));
+        vector<int> b=a;
+        next_permutation(a.begin(),a.end());
+        while(a!=b){
+            if(a[0]==0){
+                next_permutation(a.begin(),a.end());
+                continue;
+            }
+            o=0;
+            for(auto x:a){
+                o=o*10+x;
+            }
+            if(pow(2,floor(log(o)/log(2)))==o) return true;
+            next_permutation(a.begin(),a.end());
+        }
+        return false;
     }
 };
