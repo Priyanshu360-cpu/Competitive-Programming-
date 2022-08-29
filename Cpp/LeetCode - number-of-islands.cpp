7013866157bafa9d2b3718f57d31da776daba7c9
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-08-29 17:24:05 +0530

    Update LeetCode - number-of-islands.cpp

--- Cpp/LeetCode - number-of-islands.cpp
+++ Cpp/LeetCode - number-of-islands.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int numbered(vector<vector<int>>& dp,vector<vector<char>> grid,int i,int j){
-        if(i>grid.size()||j>grid[0].size()) return 0;
-        if(i<0||j<0) return 0;
+        if(i>grid.size()||j>grid[0].size()) return 1;
+        if(i<0||j<0) return 1;
         if(grid[i][j]=='0')return 0;
         if(dp[i][j]!=0) return dp[i][j];
         int a=0;
@@ -15,11 +15,11 @@ public:
             a+=1;
             dp[i][j]+=numbered(dp,grid,i+1,j);
         }
-        if(i-1>=0&&grid[i-1][j]){
+        if(i-1>=0&&grid[i-1][j]=='1'){
             a+=1;
             dp[i][j]+=numbered(dp,grid,i-1,j);
         }
-        if(j-1>=0&&grid[i][j]=='1'){
+        if(j-1>=0&&grid[i][j-1]=='1'){
             a+=1;
             dp[i][j]+=numbered(dp,grid,i,j-1);
             
