9c12e031415d940696b8da47c101abbae7d1d3f3
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-08-29 22:32:26 +0530

    Update LeetCode - find-missing-observations.cpp

--- Cpp/LeetCode - find-missing-observations.cpp
+++ Cpp/LeetCode - find-missing-observations.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void doit(vector<int>& rolls,vector<int>& out,vector<int> o,int mean,int n,int* g){
+    void doit(vector<int>& rolls,vector<int>& out,vector<int>& o,int mean,int n,int* g){
         if(*g==1)return;
         float f=accumulate(rolls.begin(),rolls.end(),0);
         float h=accumulate(o.begin(),o.end(),0);
@@ -14,7 +14,7 @@ public:
                  return;
              }
             for(int i=0;i<o.size();i++){
-                   if(o[i]==6)continue;
+                if(o[i]==6)continue;
                 o[i]+=1;
                 doit(rolls,out,o,mean,n,g);
             }
@@ -23,6 +23,7 @@ public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
         vector<int> out;
         int a=accumulate(rolls.begin(),rolls.end(),0);
+          if((a+n)/(rolls.size()+n)>mean)return out;
           vector<int> o(n,1);
             int g=0;
           doit(rolls,out,o,mean,n+rolls.size(),&g);
