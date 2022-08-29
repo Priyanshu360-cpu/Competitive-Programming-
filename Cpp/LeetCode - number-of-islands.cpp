9cb4bf03c490dce026a266bdc78b5f4eaf803be1
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-08-29 17:22:08 +0530

    Update LeetCode - number-of-islands.cpp

--- Cpp/LeetCode - number-of-islands.cpp
+++ Cpp/LeetCode - number-of-islands.cpp
@@ -6,35 +6,34 @@ public:
         if(grid[i][j]=='0')return 0;
         if(dp[i][j]!=0) return dp[i][j];
         int a=0;
+        grid[i][j]='0';
         if(j+1<grid[0].size()&&grid[i][j+1]=='1'){
-                        grid[i][j]='0';
             a+=1;
             dp[i][j]+=numbered(dp,grid,i,j+1);
-            dp[i][j+1]=0;
         }
         if(i+1<grid.size()&&grid[i+1][j]=='1'){
-                        grid[i][j]='0';
             a+=1;
             dp[i][j]+=numbered(dp,grid,i+1,j);
-            dp[i+1][j]=0;
         }
         if(i-1>=0&&grid[i-1][j]){
             a+=1;
             dp[i][j]+=numbered(dp,grid,i-1,j);
-            dp[i-1][j]=0;
         }
         if(j-1>=0&&grid[i][j]=='1'){
             a+=1;
             dp[i][j]+=numbered(dp,grid,i,j-1);
-            dp[i][j-1]=0;
+            
         }
         if(a==0)
-            dp[i][j]=0;
+            dp[i][j]=1;
         return dp[i][j];
     }
     int numIslands(vector<vector<char>>& grid) {
         vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),0));
         int s=0;
+        for(int i=0;i<grid.size();i++){
+        for(int j=0;j<grid[0].size();j++)cout<<dp[i][j];
+        }
         s=s+numbered(dp,grid,0,0);
         return s||1;
     }
