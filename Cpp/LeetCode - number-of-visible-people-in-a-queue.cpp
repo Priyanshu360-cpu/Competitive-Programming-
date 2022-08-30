7b9a58fa2e9332ce163ecf40b04fa218b25c2d46
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-08-30 17:49:07 +0530

    Update LeetCode - number-of-visible-people-in-a-queue.cpp

--- Cpp/LeetCode - number-of-visible-people-in-a-queue.cpp
+++ Cpp/LeetCode - number-of-visible-people-in-a-queue.cpp
@@ -1,3 +1,26 @@
+//Stack Solution
+
+class Solution {
+public:
+    vector<int> canSeePersonsCount(vector<int>& heights) {
+        vector<int> a(heights.size(),0);
+        stack<int> s;
+        s.push(heights[heights.size()-1]);
+        for(int i=heights.size()-2;i>=0;i--){
+            int f=0;
+            while(!s.empty()&&s.top()<=heights[i]){
+                f++;
+                s.pop();}
+            if(s.empty())a[i]=f;
+            else a[i]=f+1;
+            s.push(heights[i]);
+        }
+        return a;
+    }
+};
+
+//Brute Force Method
+
 class Solution {
 public:
     vector<int> canSeePersonsCount(vector<int>& heights) {
