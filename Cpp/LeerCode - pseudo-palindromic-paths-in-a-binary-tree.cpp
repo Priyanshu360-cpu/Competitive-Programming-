b67e5bd7aa0b77cbec294b9d47e149e662e2e2f6
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-09-14 12:18:23 +0530

    Update LeerCode - pseudo-palindromic-paths-in-a-binary-tree.cpp

--- Cpp/LeerCode - pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ Cpp/LeerCode - pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -38,3 +38,29 @@ public:
         return p;
     }
 };
+// Alternate way (Less Memory)
+class Solution {
+public:
+    map<int,int> c;
+    void solution(int *p,TreeNode* root){
+        if(!root)return;
+        c[root->val]+=1;
+        if(!root->left&&!root->right){
+            int z=0;
+            for(int i=0;i<=9;i++){
+                if(c[i]%2!=0)z+=1;
+            }
+            if(z<=1){
+                *p=*p+1;
+            }
+        }
+        solution(p,root->left);
+        solution(p,root->right);
+        c[root->val]-=1;
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        int p=0;
+        solution(&p,root);
+        return p;
+    }
+};
