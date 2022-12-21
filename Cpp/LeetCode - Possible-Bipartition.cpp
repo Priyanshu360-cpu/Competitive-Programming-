ac484dff0475eeb1f5ac1801c5e33438fa5d55f6
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-12-21 21:19:43 +0530

    Update LeetCode - Possible-Bipartition.cpp

--- Cpp/LeetCode - Possible-Bipartition.cpp
+++ Cpp/LeetCode - Possible-Bipartition.cpp
@@ -1,3 +1,39 @@
+// Working Code Color Algorithm
+class Solution {
+public:
+map<int,vector<int>> a; // The graph
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        vector<int> color(n+1,-1);
+        for(auto x:dislikes){
+            a[x[0]].push_back(x[1]);
+            a[x[1]].push_back(x[0]);
+        }// Connecting the Graph
+        color[1]=1;
+        queue<int> b;
+        b.push(1);
+        R://a goto loop to visit unconnected nodes
+        while(!b.empty()){
+            for(auto x:a[b.front()]){
+                if(color[x]==-1){
+                    color[x]=color[b.front()]==0?1:0;
+                    b.push(x);//coloring the nodes
+                }else if(color[x]==color[b.front()]){
+                    return false;//similar neighbor returns false
+                }
+            }
+            b.pop();
+        }
+        for(int i=1;i<=n;i++){
+            if(color[i]==-1&&a.find(i)!=a.end()){//find if node exists 
+                color[i]=1;
+                b.push(i);
+                goto R;//visiting unconnected nodes;
+            }
+        }
+        return true;
+    }
+};
+// Raw Code
 class Solution {
 public:
 map<int,vector<int>> a;
